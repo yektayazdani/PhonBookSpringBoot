14260ec23af7a7fdd1b5e494fbb872ca8b1214ef
Yekta Yazdani <yekta.yazdani@sikkasoftware.com>
2018-02-27 16:55:27 -0800

    CORS error rectified

--- src/main/java/com/phonebook/user/email/EmailController.java
+++ src/main/java/com/phonebook/user/email/EmailController.java
@@ -3,6 +3,7 @@ package com.phonebook.user.email;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,31 +18,31 @@ public class EmailController {
 	private EmailService emailservice;
 	
 	
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/contacts/{contactid}/emails")
 	public List<Email> getAllEmails(@PathVariable Integer contactid)
 	{
 		return emailservice.getAllEmails(contactid);
 	}
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/contacts/{contactid}/emails/{id}")
 	public Email getEmailbyId(@PathVariable Integer id)
 	{
 		return emailservice.getEmailbyId(id);
 	}
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(method= RequestMethod.POST, value = "/contacts/{contactid}/emails")
 	public void addEmailtoContacts(@PathVariable Integer contactid, @RequestBody Email email) {
 		email.setContact(new Contacts(contactid,"","",""));
 		emailservice.addEmailforContact(email);
 	}
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(method = RequestMethod.PUT, value = "/contacts/{contactid}/emails/{id}")
 	public void updateEmailId(@PathVariable Integer contactid, @RequestBody Email email){
 		email.setContact(new Contacts(contactid,"","",""));
 		emailservice.addEmailforContact(email);
 	}
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(method = RequestMethod.DELETE, value="/contacts/{contactid}/emails/{id}")
 	public void deleteEmailId(@PathVariable Integer id){
 		emailservice.deleteEmailId(id);
